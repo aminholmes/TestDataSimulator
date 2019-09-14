3f68c928b809015aebcd5789723dfdf16714b60e
Amin <aholmes@synsormed.com>
2019-09-14 17:18:44 -0400

    updates

--- server.js
+++ server.js
@@ -1,11 +1,94 @@
 var http = require('http');
+var moment = require('moment');
 //loads http module
 var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
+
+	function randomIntFromInterval(min, max) { // min and max included 
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+
+	var dataType = null;
+	var sampleData = {};
+	var numDataPoints = 15;
+	var currentPoint = {};
+	var currentDate = null;
+
+
+	//dataType options = BP, SPO2, Weight
+	dataType = 'BP';
+
+	if(dataType == 'BP'){
+		sampleData["blood pressure"] = [];
+		var currentSys = 0;
+		var currentDia = 0;
+		var currentReading = "";
+
+		for(i=0; i<numDataPoints; i++){
+			currentPoint = {};
+			currentSys = randomIntFromInterval(115,130);
+			currentDia = randomIntFromInterval(75,90);
+			currentReading = currentSys.toString() + "/" + currentDia.toString();
+
+			if(currentDate){
+				currentDate = moment(currentDate).subtract(1,'days');
+			}else{
+				currentDate = moment();
+			}
+
+
+			currentPoint["quantity"] = currentReading;
+			currentPoint["endDate"] = currentDate;
+
+			sampleData["blood pressure"].push(currentPoint);
+		};
+	}else if(dataType == "Weight"){
+		sampleData["weight"] = [];
+		var currentWeight = 0;
+
+		for(i=0;i<numDataPoints;i++){
+			currentPoint = {};
+			currentWeight = randomIntFromInterval(140,160);
+
+			if(currentDate){
+				currentDate = moment(currentDate).subtract(1,'days');
+			}else{
+				currentDate = moment();
+			}
+
+			currentPoint["quantity"] = currentWeight.toString();
+			currentPoint["endDate"] = currentDate;
+
+			sampleData["weight"].push(currentPoint);
+		}
+	}else if(dataType == "SPO2"){
+		sampleData["spo2"] = [];
+		var currentSPO2 = 0;
+
+		for(i=0;i<numDataPoints;i++){
+			currentPoint = {};
+			currentSPO2 = randomIntFromInterval(89,99);
+
+			if(currentDate){
+				currentDate = moment(currentDate).subtract(1,'days');
+			}else{
+				currentDate = moment();
+			}
+
+			currentPoint["quantity"] = currentSPO2.toString();
+			currentPoint["endDate"] = currentDate;
+
+			sampleData["spo2"].push(currentPoint);
+		}
+	}
+
+	console.log("**** the end sample data is: " + JSON.stringify(sampleData));
+
+
   //sets the right header and status code
-  res.end('Hello World\n');
+  res.end(JSON.stringify(sampleData));
   //outputs string with line end symbol
-}).listen(1337, "127.0.0.1");
+}).listen(8080, "127.0.0.1");
 //sets port and IP address of the server
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:8080/');
--- web.js
+++ web.js
@@ -3,8 +3,49 @@ var http = require('http');
 var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
+
+
+function randomIntFromInterval(min, max) { // min and max included 
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var dataType = null;
+var sampleData = {};
+var numDataPoints = 15;
+var currentPoint = {};
+var currentDate = null;
+
+dataType = 'BP';
+
+if(dataType == 'BP'){
+	sampleData["blood pressure"] = [];
+	var currentSys = 0;
+	var currentDia = 0;
+	var currentReading = "";
+
+	for(i=0; i<numDataPoints; i++){
+		currentPoint = {};
+		currentSys = randomIntFromInterval(115,130);
+		currentDia = randomIntFromInterval(75,90);
+		currentReading = currentSys.toString() + "/" + currentDia.toString();
+
+		if(currentDate){
+			currentDate = moment(currentDate).subtract(1,'days');
+		}else{
+			currentDate = moment();
+		}
+
+
+		currentPoint["quantity"] = currentReading;
+		currentPoint["endDate"] = currentDate;
+
+		sampleData["blood pressure"].push(currentPoint);
+	};
+}
+
+console.log("**** the end sample data is: " + JSON.stringify(sampleData));
   //sets the right header and status code
-  res.end('Hello World\n');
+  res.end(sampleData);
   //outputs string with line end symbol
 });
 var port = process.env.PORT || 5000;
@@ -13,4 +54,4 @@ app.listen(port, function() {
 });
 // }).listen(1337, "127.0.0.1");
 //sets port and IP address of the server
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:8080/');
